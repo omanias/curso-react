5f0feea5e5d7b9209f39feb7efdad26f3ecf154f
Omar Jesús Maniás <ojmanias1985@gmail.com>
2023-12-05 12:03:32 -0300

    modifico mensaje

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ const App = () => {
   return (
     <div>
       <NavBar />
-      <ItemListContainer greeting={"Bienvenidos a mi ecommerce"} />
+      <ItemListContainer greeting={"Bienvenidos a mi ecommerce en react"} />
     </div>
   )
 }
